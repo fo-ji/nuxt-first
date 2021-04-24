35a06489bfa5856f9b0d53e9ad1179838cc14f2b
fo-ji <fumiya1753@gmail.com>
2021-04-24 23:02:19 +0900

    initital commit

---
+++ pages/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <h2>{{ subtitle }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      subtitle: 'この文章はNuxt.jsの基本的な使用法について説明しています。',
+    }
+  },
+}
+</script>
---
+++ pages/users/_id.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>username: {{ user.name }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: this.$route.params.id,
+      users: [
+        {
+          id: 1,
+          name: 'takeshi',
+        },
+        {
+          id: 2,
+          name: 'satoshi',
+        },
+        {
+          id: 3,
+          name: 'masashi',
+        },
+      ],
+    }
+  },
+  computed: {
+    user() {
+      return this.users.find((user) => user.id == this.id)
+    },
+  },
+}
+</script>
